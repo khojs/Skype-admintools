966eac1e5f08ea12eb0b8f92d79242a3e05c748a
khojs <39031496+khojs@users.noreply.github.com>
2023-08-10 11:37:40 +0530

    Add files via upload

---
+++ Skype_SQL_DB.ps1
@@ -0,0 +1,11 @@
+﻿
+$server = $env:computername 
+$object = Get-service -ComputerName sfb  | where {($_.name -like "MSSQL$*" -or $_.name -like "MSSQLSERVER" -or $_.name -like "SQL Server (*") }
+if ($object)
+{
+$instDetails= $object |select Name,Status
+$instDetails
+}else
+{
+Write-Host "0 SQL Server instances discovered"
+}
