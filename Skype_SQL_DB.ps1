f8b4ff3cefe74f4590ab554ff4ce5a9ecfa5c276
khojs <39031496+khojs@users.noreply.github.com>
2023-08-11 11:36:38 +0530

    Add files via upload
    Latest Version changed the variable

--- Skype_SQL_DB.ps1
+++ Skype_SQL_DB.ps1
@@ -1,11 +1,11 @@
 ﻿
 $server = $env:computername 
-$object = Get-service -ComputerName sfb  | where {($_.name -like "MSSQL$*" -or $_.name -like "MSSQLSERVER" -or $_.name -like "SQL Server (*") }
+$object = Get-service -ComputerName $server | where {($_.name -like "MSSQL$*" -or $_.name -like "MSSQLSERVER" -or $_.name -like "SQL Server (*") }
 if ($object)
 {
 $instDetails= $object |select Name,Status
 $instDetails
 }else
 {
-Write-Host "0 SQL Server instances discovered"
+Write-Host "       Zero(0) SQL Server instances discovered" -ForegroundColor Green
 }
